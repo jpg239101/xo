9434c6e6e1065478771f6e2e3709373882efc5d4
jg101 <jpg239101@gmail.com>
2016-12-16 14:27:44 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+     Scanner sc = new Scanner(System.in);
+
+        System.out.println("Give a coordinate on the board \n  1 2 3 \n1| | | |\n2| | | |\n3| | | |");
+        
+    }
+}
